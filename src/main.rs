0ac83db86d75bc45b04200aa51bdb4e732505e25
Setyo <nsetyo.mail@gmail.com>
2024-01-07 21:38:37 +0700

    Min 07 Jan 2024 09:38:37  WIB

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,29 +9,9 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+        process::exit(1);
     }
 }
